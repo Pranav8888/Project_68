b6dd7da4bbf12fe703e92e634bbbced85358ed61
Pranav8888 <balamuralipranav@gmail.com>
2021-11-26 17:17:45 -0600

    First Commit

--- App.js
+++ App.js
@@ -1,21 +1,22 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Image } from 'react-native';
+import { createAppContainer } from 'react-navigation';
+import { createBottomTabNavigator } from 'react-navigation-tabs';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+import FBScreen from './screens/fb';
+import INScreen from './screens/in';
+
+export default class App extends React.Component {
+  render(){
+    return (
+        <AppContainer />
+    );
+  }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+const TabNavigator = createBottomTabNavigator({
+  Facebook: {screen: FBScreen},
+  Instagram: {screen: INScreen},
 });
+
+const AppContainer =  createAppContainer(TabNavigator);
